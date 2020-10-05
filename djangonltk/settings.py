39b6aff63d39ab56a699fbf55301185ddad0603e
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-05 12:19:29 -0500

    Cors settings

--- djangonltk/settings.py
+++ djangonltk/settings.py
@@ -140,4 +140,11 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://djangonltk.herokuapp.com/",
+]
+
+
 django_heroku.settings(locals())
