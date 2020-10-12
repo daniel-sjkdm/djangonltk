e7d265e36fa9cfa0c47bb5e7601dd360643dce89
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-12 02:08:51 -0500

    allowed origins

--- djangonltk/settings.py
+++ djangonltk/settings.py
@@ -62,8 +62,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
-
 ROOT_URLCONF = 'djangonltk.urls'
 
 TEMPLATES = [
@@ -143,8 +141,13 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
 ]
 
 
