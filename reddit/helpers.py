6c0ae5beef2d82adbda67b7d3033ac7f5fed4931
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-15 05:01:19 -0500

    Reddit search api function

--- reddit/helpers.py
+++ reddit/helpers.py
@@ -4,81 +4,107 @@ import datetime
 import json
 import praw
 import os
-
-
-
-load_dotenv()
-client_id = os.environ.get("CLIENT_ID", None)
-client_secret = os.environ.get("CLIENT_SECRET", None)
-user_agent = os.environ.get("USERAGENT", None)
-
-
-reddit = praw.Reddit(
-    client_id=client_id,
-    client_secret=client_secret,    
-    user_agent=user_agent
+from pprint import pprint
+from posts.data_processing import (
+    get_sentiment_score
 )
 
 
 def get_readable_datetime(timestamp):
-  return datetime.datetime.fromtimestamp(timestamp).strftime("%d/%m/%Y %H:%M:%S")
+    return datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
 
 
-def get_posts(subreddit, write_json=False):
+def get_reddit_client():
+    load_dotenv()
+    return praw.Reddit(
+        client_id=os.environ.get("CLIENT_ID", None),
+        client_secret=os.environ.get("CLIENT_SECRET", None),
+        user_agent=os.environ.get("USERAGENT", None)
+    )
+
+
+def search_for_subreddit(subreddit, kind, limit):
     posts = []
+    reddit = get_reddit_client()
 
-    posts_reddit = reddit.subreddit(subreddit).hot(limit=100)
-
-    for post in posts_reddit:
-        comments = []
-        for comment in post.comments:
-            if isinstance(comment, MoreComments):
-                continue
-            comments.append(comment.body)
-
+    if kind == "top":
         try:
-            posts.append({
-                "id": post.id, 
-                "author": post.author.name,
-                "subreddit": post.subreddit.display_name,
-                "created": get_readable_datetime(post.created_utc),
-                "distinguished": post.distinguished,
-                "fullname": post.fullname,
-                "downs": post.downs,
-                "num_comments": post.num_comments,
-                "title": post.title,
-                "view_count": post.view_count,
-                "ups": post.ups,
-                "url": post.url,
-                "comments": comments,
-            })
+            reddit_posts = reddit.subreddit(subreddit).top(limit=limit)
+            for post in reddit_posts:
+                comments = {}
+                for i, comment in enumerate(post.comments):
+                    if isinstance(comment, MoreComments):
+                        continue
+                    body = comment.body
+                    comments[i] = {
+                        "body": body,
+                        "sentiment": get_sentiment_score(body)
+                    }
 
-        except AttributeError:
+                posts.append({
+                    "id": post.id,
+                    "username": post.author.name,
+                    "subreddit": post.subreddit.display_name,
+                    "created": get_readable_datetime(post.created_utc),
+                    "ups": post.ups, 
+                    "num_comments": post.num_comments,
+                    "title": post.title,
+                    "url": post.url,
+                    "comments": comments,
+                    "sentiment": get_sentiment_score(post.title)
+                })
+
+        except:
+            pass
+    
+    elif kind == "hot":
+        try:
+            reddit_posts = reddit.subreddit(subreddit).hot(limit=limit)
+            for post in reddit_posts:
+                comments = {}
+                for i, comment in enumerate(post.comments):
+                    if isinstance(comment, MoreComments):
+                        continue
+                    comments[i] = comment.body
+
+                posts.append({
+                    "id": post.id,
+                    "author": post.author.name,
+                    "subreddit": post.subreddit.display_name,
+                    "created": get_readable_datetime(post.created_utc),
+                    "ups": post.ups, 
+                    "downs": post.downs,
+                    "num_comments": post.num_comments,
+                    "title": post.title,
+                    "url": post.url
+                })  
+
+        except: 
             pass
 
-    if write_json:
+    elif kind == "new":
+        try:
+            reddit_posts = reddit.subreddit(subreddit).new(limit=limit)
+            for post in reddit_posts:
+                comments = {}
+                for i, comment in enumerate(post.comments):
+                    if isinstance(comment, MoreComments):
+                        continue
+                    comments[i] = comment.body
 
-        if os.path.exists("posts.json"):
-            with open("posts.json", "r") as f:
-                json_posts = json.load(f)
-                json_posts.extend(posts)
-                with open("posts.json", "w") as f:
-                    json.dump(json_posts, f, indent=4)
-        else:
-            with open("posts.json", "w") as f:
-                json.dump(posts, f, indent=4)
-
+                posts.append({
+                    "id": post.id,
+                    "author": post.author.name,
+                    "subreddit": post.subreddit.display_name,
+                    "created": get_readable_datetime(post.created_utc),
+                    "ups": post.ups, 
+                    "downs": post.downs,
+                    "num_comments": post.num_comments,
+                    "title": post.title,
+                    "url": post.url
+                })  
+                
+        except:
+            pass
+    
     return posts
-
-
-def get_redditors():
-    redditors = []
-    for redditor in reddit.redditors.popular(limit=100):
-        redditors.append({
-            "id": redditor.id,
-            "name": redditor.display_name,
-            "created": get_readable_datetime(redditor.created_utc),
-            "description": redditor.description,
-            "subcribers": redditor.subscribers
-        })
-    return redditors
